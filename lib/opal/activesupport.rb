266ce6179ce1238e377a71deaaf826fa2c204cc6
Elia Schito <elia@schito.me>
2013-05-29 18:02:20 +0200

    Require opal stuff from /opal

--- lib/opal/activesupport.rb
+++ lib/opal/activesupport.rb
@@ -2,4 +2,4 @@ require 'opal'
 require 'opal/activesupport/version'
 
 # Just register our opal code path with opal build tools
-Opal.append_path File.join(File.dirname(__FILE__), '..', 'assets', 'javascripts')
+Opal.append_path File.expand_path('../../../opal', __FILE__)
