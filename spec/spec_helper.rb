6661446271c2b75463d68bc48c7fcbc84fad18c2
Adam Jahn <ajjahn@gmail.com>
2015-10-07 21:48:02 -0400

    Implement Module#remove_method & #redefine_method
    + spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,10 @@ require 'opal-activesupport'
 require 'active_support/inflector'
 
 module TestUnitHelpers
+  def assert actual
+    expect(actual).to be_truthy
+  end
+
   def assert_equal actual, expected
     actual.should == expected
   end
