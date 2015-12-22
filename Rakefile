84fa86a19bf55bbe4890d18c0d88f8104a7cc0e5
Elia Schito <elia@schito.me>
2015-12-22 01:14:43 +0100

    Switch to minitest

--- Rakefile
+++ Rakefile
@@ -1,7 +1,13 @@
 require 'bundler'
 Bundler.require
 
-require 'opal/rspec/rake_task'
-Opal::RSpec::RakeTask.new(:default)
+require 'opal/minitest/rake_task'
+# Opal::Minitest::RakeTask.new
+
+task :test do
+  sh "bundle exec ruby -r opal/minitest -S opal -Dwarning -Itest -Iopal test/**/*_test.rb"
+end
+
+task default: :test
 
 require 'bundler/gem_tasks'
---
+++ test/abstract_unit.rb
@@ -0,0 +1,34 @@
+# ORIG_ARGV = ARGV.dup
+#
+# begin
+#   old, $VERBOSE = $VERBOSE, nil
+#   require File.expand_path('../../../load_paths', __FILE__)
+# ensure
+#   $VERBOSE = old
+# end
+#
+# require 'active_support/core_ext/kernel/reporting'
+# require 'active_support/core_ext/string/encoding'
+#
+# silence_warnings do
+#   Encoding.default_internal = "UTF-8"
+#   Encoding.default_external = "UTF-8"
+# end
+#
+# require 'active_support/testing/autorun'
+# require 'empty_bool'
+#
+# ENV['NO_RELOAD'] = '1'
+# require 'active_support'
+#
+# Thread.abort_on_exception = true
+#
+# # Show backtraces for deprecated behavior for quicker cleanup.
+# ActiveSupport::Deprecation.debug = true
+
+require 'minitest/autorun'
+require 'empty_bool'
+require 'active_support'
+
+class ActiveSupport::TestCase < Minitest::Test
+end
