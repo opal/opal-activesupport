23bd99a30988c93e84e52441394600076fa7c084
Elia Schito <elia@schito.me>
2015-12-23 01:02:06 +0100

    Remove specs as we now use the original suite

--- spec/core_ext/kernel_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'Kernel' do
-  it 'adds class_eval to Object' do
-    o = Object.new
-    class << o; @x = 1; end
-    assert_equal 1, o.class_eval { @x }
-  end
-end
