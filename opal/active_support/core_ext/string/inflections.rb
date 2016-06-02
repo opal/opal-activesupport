51dbe1ab62f72d795c184a0565fac2e2607c77fc
balmoral <colgunn@me.com>
2016-06-02 11:19:05 +1000

    balmoral changes

--- opal/active_support/core_ext/string/inflections.rb
+++ opal/active_support/core_ext/string/inflections.rb
@@ -2,7 +2,14 @@ require 'active_support/inflector'
 
 class String
   def pluralize
-    ActiveSupport::Inflector.pluralize(self)
+    p = ActiveSupport::Inflector.pluralize(self)
+
+    msg = "#{__FILE__}[#{__LINE__}] : String#pluralize : '#{self}' => '#{p}' "
+    if RUBY_PLATFORM == 'opal'
+      `console.log(msg`
+    else
+      puts msg
+    end
   end
 
   def singularize
